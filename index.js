66a584fbe11e572a8487e3a03b525372305afe29
Mark Bien <48498353+markbien@users.noreply.github.com>
2023-06-24 19:10:25 +0800

    Add function to operate on 2 numbers and output answer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // global variables
 const output = document.querySelector(".output");
+const answer = document.querySelector('.answer');
 let firstNum = 0,
   secondNum = 0;
 let currentOperation = "";
@@ -58,6 +59,10 @@ function setOutputTextToZero() {
   output.textContent = "0";
 }
 
+function setAnswerTextToZero() {
+  answer.textContent = "0";
+}
+
 function reset() {
   firstNum = 0;
   secondNum = 0;
@@ -71,50 +76,52 @@ document.querySelectorAll("button.number").forEach((btn) => {
 const ac = document.querySelector("button[data-key='ac']");
 ac.addEventListener("click", function () {
   setOutputTextToZero();
+  setAnswerTextToZero();
   reset();
 });
 
 
+function operate(a, b, operation) {
+  let total = 0;
+  a = Number(a);
+  b = Number(b);
+  switch (operation) {
+    case "+":
+      total = a + b;
+      break;
+    case "-":
+      total = a - b;
+      break;
+    case "*":
+      total = a * b;
+      break;
+    case "/":
+      if (b === 0) {
+        return "You can't divide by 0!";
+      }
+      total = a / b;
+      break;
+  }
+  return total;
+}
 
-// function operate(a, b, operation) {
-//   let total = 0;
-//   a = Number(a);
-//   b = Number(b);
-//   switch (operation) {
-//     case "+":
-//       total = a + b;
-//       break;
-//     case "-":
-//       total = a - b;
-//       break;
-//     case "*":
-//       total = a * b;
-//       break;
-//     case "/":
-//       if (b === 0) {
-//         return "You can't divide by 0!";
-//       }
-//       total = a / b;
-//       break;
-//   }
-//   return total;
-// }
+function setOperation(){
+  currentOperation = this.textContent; // Set the currentOperation variable to the current operation clicked
+  firstNum = output.textContent; // Sets firstNum to the output's text content
+  answer.textContent = firstNum; // Sets the answer's text content to firstNum
+  setOutputTextToZero();
+}
 
-// function setOperation(){
-//   currentOperation = this.textContent;
-//   firstNum = output.textContent;
-//   setOutputTextToZero();
-// }
+function getTotal(){
+  secondNum = output.textContent;
+  answer.textContent = operate(firstNum, secondNum, currentOperation);
+  setOutputTextToZero();
+}
 
-// function getTotal(){
-//   secondNum = output.textContent;
-//   output.textContent = operate(firstNum, secondNum, currentOperation);
-// }
+const operations = document.querySelectorAll('.operations');
+operations.forEach(button => {
+  button.addEventListener('click', setOperation);
+});
 
-// const operations = document.querySelectorAll('.operations');
-// operations.forEach(button => {
-//   button.addEventListener('click', setOperation);
-// });
-
-// const totalButton = document.querySelector('.total');
-// totalButton.addEventListener('click', getTotal);
+const totalButton = document.querySelector('.total');
+totalButton.addEventListener('click', getTotal);
